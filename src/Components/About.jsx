a9745470ebac6680b46c08fbd9237b842424c9eb
Mohitsharma <mohitkumarsharma40@gmail.com>
2022-10-19 17:42:25 +0530

    Added

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -43,7 +43,15 @@ const About = () => {
                     </div>
                     <div className='right'>
                         <h2>I am <span>Full Stack Web Developer</span></h2>
-                        <p>My name is Mohit Sharma, I am from Rishikesh, Uttarakhand. I have completed BTECH & have knowledge of HTML, CSS , Javascript, React, Redux, NodeJs, MongoDB. I want to work as a Full Stack Web Developer.</p>
+                        <p>My name is Mohit Sharma, I am from Rishikesh, Uttarakhand. Passionate Full Stack Web Developer
+                            with skills in the MERN stack, I have
+                            learned Full Stack Web Development
+                            in Masai school. Skilled in problem-
+                            solving and execution of software
+                            tasks from start to finish. I look
+                            forward to joining a company where
+                            I can contribute to individual and
+                            company growth..</p>
                         <div>
                             <img src={signature} alt='Mohit_Signature' width='200px' />
                         </div>
